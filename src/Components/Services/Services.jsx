dd1db14bb3d8f96dbb81e2a66b442741224b884f
RenanTCastro <renancastrotut@gmail.com>
2023-08-12 12:14:46 -0300

    componente de serviços

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./Services.css";
+
+export default function Services() {
+    
+    return (
+        <div className="services-container">
+            <h1 className="services-title">Conheça meus serviços</h1>
+            <div className="services-cards-container">
+                <button className="services-card-button"> - </button>
+                <div className="services-card">
+                    <img className="services-card-image" src="https://www.florence.edu.br/blog/wp-content/uploads/2021/08/Florence-dia-do-nutricionista.jpg" alt="Foto"/>
+                    <h4>Endocrinologia e Metabologia</h4>
+                    <p>Endocrinologia é a especialidade médica que estuda as ordens do sistema endócrino e suas secreções específicas,
+                        chamadas de secreções fisiológicas.</p>
+                </div>
+                <div className="services-card">
+                    <img className="services-card-image" src="https://www.florence.edu.br/blog/wp-content/uploads/2021/08/Florence-dia-do-nutricionista.jpg" alt="Foto"/>
+                    <h4>Endocrinologia e Metabologia</h4>
+                    <p>Endocrinologia é a especialidade médica que estuda as ordens do sistema endócrino e suas secreções específicas,
+                        chamadas de secreções fisiológicas.</p>
+                </div>
+                <div className="services-card">
+                    <img className="services-card-image" src="https://www.florence.edu.br/blog/wp-content/uploads/2021/08/Florence-dia-do-nutricionista.jpg" alt="Foto"/>
+                    <h4>Endocrinologia e Metabologia</h4>
+                    <p>Endocrinologia é a especialidade médica que estuda as ordens do sistema endócrino e suas secreções específicas,
+                        chamadas de secreções fisiológicas.</p>
+                </div>
+                <button className="services-card-button">+</button>
+            </div>
+            <button className="services-contact-button">ENTRAR EM CONTATO</button>
+        </div>
+    );
+}
+ 
--- src/Pages/ModeloNutri/ModeloNutri.jsx
+++ src/Pages/ModeloNutri/ModeloNutri.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import Header from "../../Components/Header/Header";
 import About from "../../Components/About/About";
+import Services from "../../Components/Services/Services";
+// import Social from "../../Components/Social/Social";
 import Footer from "../../Components/Footer/Footer";
 
 export default function ModeloNutri() {
@@ -9,6 +11,8 @@ export default function ModeloNutri() {
     <div>
       <Header/>
       <About/>
+      <Services/>
+      {/* <Social/> */}
       <Footer/>
     </div>
   )
